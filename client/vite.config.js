4c1bdbc48405424a9b3e18a89e0cee0eaa7d2bed
Maxim Novikov <mthelast@gmail.com>
2026-06-04 15:38:34 +0300

    UPDATE: constants import method

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Fixed the package name here
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['../GameMechanics/constants.js'] // Force Vite to pre-bundle this file as ESM
+  },
+  resolve: {
+    alias: {
+      // Clean alias so you don't have to use messy "../../" relative paths
+      '@shared': path.resolve(__dirname, '../GameMechanics')
+    }
+  }
+});
